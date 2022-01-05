ffe207e47087957c52e6f5a815be45a3c566f8db
Marek Kotajny <mkotajny@gmail.com>
2022-01-05 11:16:14 +0100

    feat: add google Auth into sign in button
    POA-148

--- src/components/nonShared/TopBar/index.tsx
+++ src/components/nonShared/TopBar/index.tsx
@@ -1,7 +1,8 @@
-import { AppBar, Toolbar, Typography, Button, Box } from '@mui/material';
-import GoogleIcon from '@mui/icons-material/Google';
+import { AppBar, Toolbar, Typography, Box } from '@mui/material';
 import { useTranslation } from 'react-i18next';
 import { useLocation } from 'react-router';
+
+import GoogleAuth from '../GoogleAuth';
 
 import { useStyles } from './styles';
 
@@ -16,9 +17,7 @@ const TopBar = () => {
           <Typography variant='h6' component='div' sx={{ flexGrow: 1 }}>
             {t(`PAGES.${titleKey}.TITLE`)}
           </Typography>
-          <Button color='inherit' startIcon={<GoogleIcon />}>
-            {t('TOP_BAR.LOGIN')}
-          </Button>
+          <GoogleAuth />
         </Toolbar>
       </AppBar>
     </Box>
